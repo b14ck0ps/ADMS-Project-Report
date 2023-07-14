2e5f79f942fe6c2eb7da75a4608150d64811db02
Azran Hossain <mr69751@gmail.com>
2023-07-14 21:32:22 +0600

    subq'

--- RawQuery/dmlqueries.sql
+++ RawQuery/dmlqueries.sql
@@ -27,3 +27,34 @@ GROUP BY Team_Country;
 --Calculate the total salary expense for content creators
 SELECT SUM(ContentCreator_Salary) AS Total_Salary_Expense
 FROM ContentCreator;
+
+-------------- Sub Query --------------
+-- Retrieve the managers associated with teams established before a specific date
+SELECT Manager_Name
+FROM Manager
+WHERE Manager_ID IN (
+    SELECT Manager_ID
+    FROM Team
+    WHERE Team_Established_Date < TO_DATE('2022-03-01', 'YYYY-MM-DD')
+);
+
+-- Get the content creators who have a higher salary than the average salary of all content creators
+SELECT ContentCreator_Name
+FROM ContentCreator
+WHERE ContentCreator_Salary > (
+    SELECT AVG(ContentCreator_Salary)
+    FROM ContentCreator
+);
+
+-- Retrieve the teams managed by managers who have won a championship
+SELECT Team_Name
+FROM Team
+WHERE Manager_ID IN (
+    SELECT Manager_ID
+    FROM Manager
+    WHERE Manager_ID IN (
+        SELECT DISTINCT Manager_ID
+        FROM Team_Winning
+        WHERE Team_Winning LIKE '%Championship%'
+    )
+);
